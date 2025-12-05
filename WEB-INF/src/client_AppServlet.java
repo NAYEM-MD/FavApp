bb83799e2df945fff4834291ffeb90ada7b9840e
Nayem <mn25304028@ga.ttc.ac.jp>
2025-12-05 15:48:23 +0900

    JIkko okk

--- WEB-INF/src/client_AppServlet.java
+++ WEB-INF/src/client_AppServlet.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class client_AppServlet extends HttpServlet {
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
